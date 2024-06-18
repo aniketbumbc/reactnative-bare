32e17a5c262cf0de828d09edc016f0b7c05c47e4
aniket <aniket.umbc@gmail.com>
2024-06-18 06:50:15 +0530

    added location pckgs and test

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import * as Location from 'expo-location';
 
 export default function App() {
+  async function getUserLocation() {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      setErrorMsg('Permission to access location was denied');
+      return;
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    console.log(location);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to bare app working on your app!</Text>
+      <Text style={styles.textColor}>
+        Open up App.js to bare app working on your app!
+      </Text>
+      <View
+        style={{ marginTop: 10, padding: 10, borderRadius: 10, width: '40%' }}
+      >
+        <Button title='Get Location' onPress={getUserLocation} />
+      </View>
       <StatusBar style='auto' />
     </View>
   );
@@ -14,8 +33,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'green',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  textColor: {
+    backgroundColor: 'white',
+    fontSize: 24,
   },
 });
